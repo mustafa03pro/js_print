5e736997e01cf9fd5b2123fde4ab9a9bc1d6b782
Mohd Mustafa Ahmed <mustafamohdvvip@gmail.com>
2025-04-08 23:59:17 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,11 @@
+function fun1()
+{
+    document.writeln("hi  i am fun ln","i am muatafa");
+    document.write("hi  i am fun 1");
+    console.log("data");
+    
+}
+function fun2()
+{
+    document.write("I am fun 2");
+}
